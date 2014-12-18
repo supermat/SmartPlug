252c80e2534571fc9f3afaea0b6b69bd95227523
FaNiZ-08 <nizcom@aliceadsl.fr>
2014-12-18 11:05:36 +0100

    Update http_request.php

--- QueLLS/lib/http_request.php
+++ QueLLS/lib/http_request.php
@@ -6,13 +6,30 @@ function makeHTTPRequest($data) {
 	global $debug;
 	// isolation de l'url
 	$url = $data["url"];
+	unset($data["url"]);	//retire l'URL des paramètres
+	// isolation de la méthode
+	$method = "POST";
+	if (isset($data["method"])) {
+		$method = $data["method"]; 
+		unset($data["method"]);
+	}
+	// isolation du Content-Type s'il est défini
+	$content_type = "";
+	if (isset($data["Content-type"])) {
+		$content_type = "Content-type: ".$data["Content-type"]."; "; 
+		unset($data["Content-type"]);
+	}
 
 	// use key 'http' even if you send the request to https://...
 	$options = array(
 		'http' => array(
 			'header'  => "Authorization: Basic " . base64_encode("admin:admin") . "; ".
-			"Content-type: application/\r\n",
-			'method'  => 'POST',
+							$content_type,
+//			Je n'ai pas trouvé quel Content-type il fallait mettre pour que ça fontionne en appelant record_state.php...
+//			Si je laisse application/x-www-form-urlencoded, je n'ai aucun paramètre dans $_GET/$_POST
+//			En revanche, sans Conten-type, l'appel HTTP à la prise renvoie une erreur
+//			"Content-type: application/x-www-form-urlencoded\r\n",
+			'method'  => $method,
 			'content' => http_build_query($data),
 		),
 	);
