09e9de4668d360e8608714b32b567f8630683b4f
FaNiZ-08 <nizcom@aliceadsl.fr>
2014-12-18 11:07:29 +0100

    Update record_state.php

--- QueLLS/record_state.php
+++ QueLLS/record_state.php
@@ -6,12 +6,32 @@
 </HEAD>
 <BODY>  
 
+<?php
+define('BASE', './');
+require_once(BASE.'lib/debug.php');
+require_once(BASE.'lib/http_request.php');
+require_once(BASE.'lib/smartplug.php');
+
+// getURLparameter
+//	récupère un paramètre de l'URL quel que soit la méthode (GET/POST) et renvoie la valeur
+//	p_name = nom du paramètre
+function getURLparameter($p_name) {
+	$r="";
+	if (isset($_GET[$p_name])) $r=$_GET[$p_name];
+	if (isset($_POST[$p_name])) $r=$_POST[$p_name];
+	return $r;
+}
+
+$show_help_message = !(getURLparameter("nohelp")=="true");	// détermine l'affichageou non du message d'aide
+
+if ($show_help_message) {
+?>
 Paramètres d'appel de la page :
 <ul>
 <li>Mode 'save' : les informations sont collectées en amont de l'appel de cette page, la page se contente de les enregistrer dans la base de données</li>
 	<ul>
 	<li>mode = save</li>
-	<li>id = Identifiant de la prise, tel que définit dans la BDD</li>
+	<li>id = Identifiant de la prise, tel que défini dans la BDD</li>
 	<li>A = intensité en Ampères</li>
 	<li>V = tension en Volts</li>
 	<li>W = puissance en Watts</li>
@@ -30,29 +50,20 @@ Paramètres d'appel de la page :
 	</ul>  
 	<br />
 <li>Quel que soit le mode, le paramètre debug affiche des infos de débogage</li>
+<li>Le paramètre nohelp=true n'affiche pas ce message</li>
 </ul>  
 
 <?php
-define('BASE', './');
-require_once(BASE.'lib/debug.php');
-require_once(BASE.'lib/http_request.php');
-require_once(BASE.'lib/smartplug.php');
-
-// getURLparameter
-//	récupère un paramètre de l'URL quel que soit la méthode (GET/POST) et renvoie la valeur
-//	p_name = nom du paramètre
-function getURLparameter($p_name) {
-	$r="";
-	if (isset($_GET[$p_name])) $r=$_GET[$p_name];
-	if (isset($_POST[$p_name])) $r=$_POST[$p_name];
-	return $r;
 }
 
 // Initialisation des variables
 $plug_id = getURLparameter("id");					// id de la prise en base
 $ip_address = getURLparameter("ip_address");		// adresse IP de la prise
 $mode = getURLparameter("mode");  					// mode de fonctionnement : fetch = interroge la prise; save = enregistre l'état passé en paramètre de l'URL
-if ($mode=="") $mode = "fetch";			
+if ($mode=="") $mode = "fetch";	
+
+echo "Enregistrement de l'état de la prise id=$plug_id ($ip_address) <br />";		
+echo "Mode = ".$mode."<br />";
 
 if ($mode=="save") {
 	// récupération des valeurs à sauvegarder
@@ -90,7 +101,7 @@ $sql .= $voltage.", ";
 $sql .= $power.", ";
 $sql .= $power_sum;
 $sql .= ");";
-if ($debug) echo "SQL = ".$sql;
+echo "Exécution de la requête SQL = ".$sql."<br />";
 
 // exécution de la requête
 $res = mysql_query($sql, $conn);
